d5b46e36bf0926bcbec16a0f5f78e673cdbc528d
rafli zocky <raflizocky@gmail.com>
2025-02-01 13:49:27 +0700

    adjust: unused files/menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -23,7 +23,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Section 1
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
@@ -31,32 +31,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Menu 1</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/buttons.html') }}">Buttons</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/cards.html') }}">Cards</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/utilities-color.html') }}">Colors</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/utilities-border.html') }}">Borders</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/utilities-animation.html') }}">Animations</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/utilities-other.html') }}">Other</a>
+                        <h6 class="collapse-header">Submenu:</h6>
+                        <a class="collapse-item" href="#">Submenu 1</a>
+                        <a class="collapse-item" href="#">Submenu 2</a>
                     </div>
                 </div>
             </li>
@@ -66,42 +47,14 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Addons
+                Section 2
             </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/login.html') }}">Login</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/register.html') }}">Register</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/forgot-password.html') }}">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/404.html') }}">404 Page</a>
-                        <a class="collapse-item" href="{{ asset('sbadmin2/blank.html') }}">Blank Page</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ asset('sbadmin2/charts.html') }}">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="{{ asset('sbadmin2/tables.html') }}">
+                <a class="nav-link" href="/admin">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Admin</span></a>
             </li>
 
             <!-- Divider -->
@@ -110,16 +63,6 @@
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="{{ asset('sbadmin2/img/undraw_rocket.svg') }}"
-                    alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
-                    and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
-                    Pro!</a>
             </div>
 
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
